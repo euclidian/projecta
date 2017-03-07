8894d81f230a954e4eb31719e59da25d6fe003b6
Muhammad Anis <anisiconic@gmail.com>
2017-03-07 10:36:01 +0700

    Add template tags like django-require

---
+++ django_project/changes/templatetags/grunt.py
@@ -0,0 +1,23 @@
+from django import template
+from django.conf import settings
+from django.utils.safestring import mark_safe
+from django.contrib.staticfiles.storage import staticfiles_storage
+
+
+register = template.Library()
+
+@register.simple_tag
+def grunt_module(module):
+    """
+    Render A Javascript based on grunt
+    :param module:Script filename of js
+    :return: Template to render
+    """
+    if not settings.DEBUG:
+        return mark_safe(
+            """<script src={module}></script>""".format(
+                module=staticfiles_storage.url(module)
+            )
+        )
+
+    return
